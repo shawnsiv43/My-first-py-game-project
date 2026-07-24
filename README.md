44f003ebd056813ca0d0042c534252a0c4094a18
shawnsiv43 <shawnsiv43@gmail.com>
2026-07-24 18:48:51 -0400

    Added stuff

---
+++ README.md
@@ -0,0 +1,14 @@
+### MY FIRST PYTHON PYGAME PROGRAM!!!
+
+
+## How to Run
+Hit CTRL+S
+
+Click the run button or in the terminal type python and the file name
+# Features
+
+Can make circles and rectangles in your favorite color
+
+Can place the shapes randomly
+
+Has a count for each of the both shapes
--- rect_circ.py
+++ rect_circ.py
@@ -3,9 +3,9 @@ import random
 from PIL import ImageColor
 import sys
 
-color=input("Enter the color you want to change for the circle")
+color=input("Enter the color you want to change for the circle ")
 
-color2=input("Enter the color you want to change for the rectangle")
+color2=input("Enter the color you want to change for the rectangle ")
 
 
 
@@ -34,13 +34,20 @@ rect_count=0
 
 circle_count=0
 
+font_system2=pygame.font.SysFont("Arial",15)
+
+instuction=font_system2.render(f"Press c for circle\nPress r for rectangle\n press q to quit",True, (0,255,255))
+instuction_rect=instuction.get_rect(center=(65,300))
+
+screen.blit(instuction,instuction_rect)
+
 
 text_surface=font_system.render(f"circle_count: {circle_count}",True,(255,255,255))
 text_rect=text_surface.get_rect(center=(65,15))
 screen.blit(text_surface,text_rect)
-text_surface=font_system.render(f"rect_count: {rect_count}",True,(255,255,255))
-text_rect=text_surface.get_rect(center=(65,40))
-screen.blit(text_surface,text_rect)
+text_surface2=font_system.render(f"rect_count: {rect_count}",True,(255,255,255))
+text_rect2=text_surface2.get_rect(center=(65,40))
+screen.blit(text_surface2,text_rect2)
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT or (event.type==pygame.KEYDOWN and event.key==pygame.K_q):
@@ -49,22 +56,20 @@ while running:
         elif event.type==pygame.KEYDOWN:
             l=greet(color)
             if event.key==pygame.K_c:
-                text_surface=font_system.render("",True,(0,0,0))
-                text_rect=text_surface.get_rect(center=(65,15))
-                screen.blit(text_surface,text_rect)
-                pygame.draw.circle(screen, l, radius=20, center=(random.uniform(0,400),random.uniform(0,400)))
+                pygame.draw.rect(screen, (0,0,0), text_rect)
                 circle_count=count(circle_count)
                 text_surface=font_system.render(f"circle_count: {circle_count}",True,(255,255,255))
                 text_rect=text_surface.get_rect(center=(65,15))
                 screen.blit(text_surface,text_rect)
+                pygame.draw.circle(screen, l, radius=20, center=(random.uniform(130,400),random.uniform(0,400)))
             elif event.key==pygame.K_r:
                 p=greet(color2)
-                pygame.draw.rect(screen, p, (random.uniform(0,400),random.uniform(0,400),25,30))
-
+                pygame.draw.rect(screen, (0,0,0), text_rect2)
                 rect_count=count(rect_count)
-                text_surface=font_system.render(f"rect_count: {rect_count}",True,(255,255,255))
-                text_rect=text_surface.get_rect(center=(65,40))
-                screen.blit(text_surface,text_rect)
+                text_surface2=font_system.render(f"rect_count: {rect_count}",True,(255,255,255))
+                text_rect2=text_surface2.get_rect(center=(65,40))
+                screen.blit(text_surface2,text_rect2)
+                pygame.draw.rect(screen, p, (random.uniform(130,400),random.uniform(0,400),20,30))
     pygame.display.update()
     pygame.display.flip()
 
